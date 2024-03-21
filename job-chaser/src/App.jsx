c27af8b6fffb2ca056016d56fb875cba554647d1
Simon-jmk <151679315+Simon-jmk@users.noreply.github.com>
2024-03-21 23:16:51 +0100

    jobsearch api beginning of tailwind

--- job-chaser/src/App.jsx
+++ job-chaser/src/App.jsx
@@ -1,30 +1,68 @@
-import { useState } from "react";
-import "./App.css";
-import { jobs } from "./data.js";
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function App() {
-  function SearchBar() {
-    return (
-      <div>
-        <input type="search" />
-        <button>Search</button>
-      </div>
-    );
-  }
-  function SearchResults() {
-    const jobList = jobs.map((job) => (
-      <li>{job.company + " - " + job.position}</li>
-    ));
-    return (
-      <div>
-        <ul>{jobList}</ul>
-      </div>
-    );
-  }
+function SearchBar({ inputValue, onInputChange, onSearchClick }) {
   return (
     <div>
-      <SearchBar />
-      <SearchResults />
+      <input type="search" value={inputValue} onChange={onInputChange} />
+      <button onClick={onSearchClick}>Search</button>
+    </div>
+  );
+}
+
+function SearchResults({ jobs, loading, error }) {
+  const jobList = jobs.map((job) => (
+    <li className="flex align-center mb-10 border-2 border-black gap-2 h-40" key={job.id}>
+      <img className="object-contain max-w-40" src={job.logo_url} alt="" />
+      <h2 className='self-center'>{job.headline}</h2>
+    </li>
+  ));
+  return (
+    <div>
+      {loading && <p>Loading jobs...</p>}
+      {error && <p>Error fetching jobs: {error.message}</p>}
+      <ul>{jobList}</ul>
+    </div>
+  );
+}
+
+function App() {
+  const [jobs, setJobs] = useState([]);
+  const [loading, setLoading] = useState(false);
+  const [error, setError] = useState(null);
+  const [inputValue, setInputValue] = useState('');
+
+  const fetchJobs = async () => {
+    setLoading(true);
+    try {
+      const response = await axios.get(`https://jobsearch.api.jobtechdev.se/search?q=${inputValue}`);
+      setJobs(response.data.hits);
+      setError(null);
+    } catch (error) {
+      setError(error);
+    }
+    setLoading(false);
+  };
+
+  function handleInputChange(event) {
+    setInputValue(event.target.value);
+  }
+
+  function handleSearchClick() {
+    fetchJobs();
+  }
+
+  return (
+    <div>
+      <h2 className=''>hello</h2>
+      <SearchBar 
+        inputValue={inputValue} 
+        onInputChange={handleInputChange} 
+        onSearchClick={handleSearchClick} />
+      <SearchResults 
+        jobs={jobs} 
+        loading={loading} 
+        error={error} />
     </div>
   );
 }
