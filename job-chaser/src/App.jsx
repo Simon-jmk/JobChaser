d6fe6536c193564d059c7735bd85161d828a4cd8
Simon-jmk <151679315+Simon-jmk@users.noreply.github.com>
2024-03-22 15:22:11 +0100

    arbetsförmedlingen start styling

--- job-chaser/src/App.jsx
+++ job-chaser/src/App.jsx
@@ -3,17 +3,22 @@ import axios from 'axios';
 
 function SearchBar({ inputValue, onInputChange, onSearchClick }) {
   return (
-    <div>
-      <input type="search" value={inputValue} onChange={onInputChange} />
-      <button onClick={onSearchClick}>Search</button>
+    <div className='flex justify-between border-2 gap-5'>
+      <img src="https://arbetsformedlingen.se/webdav/files/logo/logo.svg" alt="" />
+      <div className='flex align-center w-96'>
+        <input className='border-2 w-full' type="search" value={inputValue} onChange={onInputChange} />
+        <button className='border-2' onClick={onSearchClick}>Search</button>
+      </div>
     </div>
   );
 }
 
 function SearchResults({ jobs, loading, error }) {
   const jobList = jobs.map((job) => (
-    <li className="flex align-center mb-10 border-2 border-black gap-2 h-40" key={job.id}>
-      <img className="object-contain max-w-40" src={job.logo_url} alt="" />
+    <li className="flex align-center mb-10 border-2  gap-2 h-40 bg-blue-900 text-white" key={job.id}>
+      <div className='flex size-40 p-4'>
+        <img className="object-contain" src={job.logo_url} alt="" />
+      </div>
       <h2 className='self-center'>{job.headline}</h2>
     </li>
   ));
@@ -53,8 +58,7 @@ function App() {
   }
 
   return (
-    <div>
-      <h2 className=''>hello</h2>
+    <div className='p-4'>
       <SearchBar 
         inputValue={inputValue} 
         onInputChange={handleInputChange} 
